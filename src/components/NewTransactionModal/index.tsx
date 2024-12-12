c1ba153a804a09ce5502dd82fab26d1464105306
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-12 18:35:29 -0300

    criando botão acessível

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -16,8 +16,8 @@ export function NewTransactionModal() {
 					<input type="number" placeholder='Preço' required />
 					<input type="text" placeholder='Categoria' required />
 					<TransactionType>
-						<TransactionTypeButton variant="income"><ArrowCircleUp size={24}/>Entrada</TransactionTypeButton>
-						<TransactionTypeButton variant="outcome"><ArrowCircleDown size={24}/>Saída</TransactionTypeButton>
+						<TransactionTypeButton value="income" variant="income"><ArrowCircleUp size={24}/>Entrada</TransactionTypeButton>
+						<TransactionTypeButton value="outcome" variant="outcome"><ArrowCircleDown size={24}/>Saída</TransactionTypeButton>
 					</TransactionType>
 					<button type="submit">Cadastrar</button>
 				</form>
