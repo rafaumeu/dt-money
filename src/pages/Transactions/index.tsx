81720032ba9140a63a3510029168f89585c67073
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-12 04:18:24 -0300

    Component searchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
 
 export function Transactions() {
   return(
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td><PriceHighlight variant="income">R$12.000,00</PriceHighlight></td>
+              <td>Categoria</td>
+              <td>Venda</td>
+              <td>21/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td><PriceHighlight variant="outcome">-R$59,00</PriceHighlight></td>
+              <td>Categoria</td>
+              <td>Alimentação</td>
+              <td>21/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
