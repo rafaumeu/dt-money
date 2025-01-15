c2e5370c568c2c3999a4bc8c4489639d832f8437
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-15 12:47:31 -0300

    feat(transport): implement transport feature and update axios configuration - Implement transport feature in the application - Update axios to use environment variable for token - Create .env file to store environment variables

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,22 @@
 import axios from 'axios'
+import dotenv from 'dotenv'
 
-export const api = axios.create({
-  baseURL:
-    'https://dt-money-api-abs5-ffygew4wn-rafaels-projects-62f31870.vercel.app/',
+dotenv.config()
+
+const token = process.env.VERCEL_TOKEN
+
+const api = axios.create({
+  baseURL: 'https://dt-money-5zs6qc4we-rafaels-projects-62f31870.vercel.app/',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 })
+
+// Adicione um interceptor para adicionar o token a todas as requisições
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+export { api }
+
